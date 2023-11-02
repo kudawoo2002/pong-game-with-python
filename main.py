da0872300f977422734cef657c473322ff63a090
kudawoo2002 <kudawoo2002@gmail.com>
2023-11-02 17:25:34 +0000

    Window code only

---
+++ main.py
@@ -0,0 +1,22 @@
+import turtle
+
+
+wn = turtle.Screen()
+wn.title("Pong Game")
+wn.setup(width=800, height=600)
+
+wn.bgcolor("#000")
+wn.tracer(0)
+
+
+# Paddle A
+
+# Paddle B
+
+# Ball
+
+
+# main game loop
+while True:
+    wn.update()
+
